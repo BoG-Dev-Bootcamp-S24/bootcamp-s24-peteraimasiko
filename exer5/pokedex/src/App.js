fb45ad500cb44c1483c771444a122847ee629fa9
Peter <paimasiko3@gatech.edu>
2024-02-20 16:12:19 -0500

    finished adding extra

--- exer5/pokedex/src/App.js
+++ exer5/pokedex/src/App.js
@@ -23,7 +23,6 @@ function App() {
     }
   }
 
-  // fetch data at beginning and whenever index changes
   useEffect(() => {
     getData();
   }, [index]);
@@ -46,6 +45,21 @@ function getTypes() {
   }
   
 }
+
+function handleShiny(event) {
+  if (event.key === "s") {
+    const image = document.getElementById("sprite")
+    try {
+      image.src = data.sprites.front_shiny;
+      document.removeEventListener("keydown", handleShiny)
+    } catch (error) {
+      return;
+    }
+    
+  }
+}
+
+
 
 function InfoMoves() {
   try {
@@ -93,12 +107,14 @@ function getInfoMoves () {
   
 }
 
+
+document.addEventListener("keydown", handleShiny);
   return (
     <div className="flex flex-col items-center">
       <h1 className='font-bold text-4xl m-9'>Exercise 5 - PokeDex!</h1>
       <div className='flex justify-evenly w-3/4'>
         <div className='flex flex-col justify-evenly'>
-          <img className='w-72 border-[3px] border-black m-4' src={getSprite()}></img>
+          <img className='w-72 border-[3px] border-black m-4' id='sprite' src={getSprite()}></img>
           <div className='text-center w-[17.7rem] border border-black py-1 bg-[#E8E8E8] mb-3 self-center text-xl rounded-md h-[38px]'>{data.name}</div>
           <p className=' font-bold'>Types:</p>
           <ul>
