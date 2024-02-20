48ecf87b43c4e54aa5fe68dd09c363088f9b5f4e
Peter <paimasiko3@gatech.edu>
2024-02-20 10:50:17 -0500

    added first items

--- exer5/pokedex/src/App.js
+++ exer5/pokedex/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import exam from './1.png'
+import sign from './lt.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center">
+      <h1 className='font-bold text-4xl'>Exercise 5 - PokeDex!</h1>
+      <div className='flex flex-col justify-evenly'>
+        <img className='w-72 border-2 border-black' src={exam}></img>
+        <div className='text-center w-64 border bg-[#E8E8E8] self-center'>Bulbasaur</div>
+        <p>Types:</p>
+        <ul>
+          <li className='normal'>normal</li>
+        </ul>
+        <div className='flex justify-evenly'>
+          <button className='bg-[#E8E8E8] w-20'><img src={sign}></img></button>
+          <button className='bg-[#E8E8E8] w-20'><img className=' rotate-180' src={sign}></img></button>
+        </div>
+      </div>
     </div>
   );
 }
