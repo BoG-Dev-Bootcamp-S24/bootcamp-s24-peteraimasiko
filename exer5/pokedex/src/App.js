03a661086758a3345eb10153ad130e2785153cf8
Peter <paimasiko3@gatech.edu>
2024-02-20 12:19:30 -0500

    working on design of info div

--- exer5/pokedex/src/App.js
+++ exer5/pokedex/src/App.js
@@ -6,16 +6,22 @@ function App() {
   return (
     <div className="flex flex-col items-center">
       <h1 className='font-bold text-4xl'>Exercise 5 - PokeDex!</h1>
-      <div className='flex flex-col justify-evenly'>
-        <img className='w-72 border-2 border-black' src={exam}></img>
-        <div className='text-center w-64 border bg-[#E8E8E8] self-center'>Bulbasaur</div>
-        <p>Types:</p>
-        <ul>
-          <li className='normal'>normal</li>
-        </ul>
-        <div className='flex justify-evenly'>
-          <button className='bg-[#E8E8E8] w-20'><img src={sign}></img></button>
-          <button className='bg-[#E8E8E8] w-20'><img className=' rotate-180' src={sign}></img></button>
+      <div className='flex justify-evenly w-2/3'>
+        <div className='flex flex-col justify-evenly'>
+          <img className='w-72 border-2 border-black' src={exam}></img>
+          <div className='text-center w-64 border bg-[#E8E8E8] self-center'>Bulbasaur</div>
+          <p>Types:</p>
+          <ul>
+            <li className='normal'>normal</li>
+          </ul>
+          <div className='flex justify-evenly'>
+            <button className='bg-[#E8E8E8] w-20'><img src={sign}></img></button>
+            <button className='bg-[#E8E8E8] w-20'><img className=' rotate-180' src={sign}></img></button>
+          </div>
+        </div>
+        <div className='flex flex-col'>
+          <h3 className='font-bold'>Moves</h3>
+          <ul className=''></ul>
         </div>
       </div>
     </div>
